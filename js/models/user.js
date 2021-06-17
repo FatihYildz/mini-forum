d0ca6ef6b7de8f570b95b863929466ed38ba6826
Fatih <yildizfatih96@gmail.com>
2021-06-17 16:53:01 +0200

    Models creation

---
+++ js/models/user.js
@@ -0,0 +1,8 @@
+export class User {
+    constructor(id, username, password, admin) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.admin = admin;
+    }
+};
