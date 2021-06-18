7c90bae26c1dd2da4bd39719ef271718328dcd4a
Pierre Chatard <chatard.pierre@gmail.com>
2021-06-18 15:02:53 +0200

    Retrieve topics on homepage and topic on topic page

--- js/displays/topicDisplay.js
+++ js/displays/topicDisplay.js
@@ -1,44 +1,35 @@
+import { Topic } from '../models/topic.js';
+
 export function createTopicDisplay() {
     const topicDisplay = document.createElement('div');
 
-    // const topic = getTopicInformations();
+    const topicId = localStorage.getItem('topicId');
+    Topic.getTopic(topic).then(topic => {
 
-    const topic = {
-        title: 'titre',
-        author: 'auteur',
-        messages: [
-            { author: 'xxx', date: '27/02', hour: '16h00', text: 'fdsfsdfdsfsdf' },
-            { author: 'xxx', date: '27/02', hour: '16h00', text: 'fdsfsdfdsfsdf' },
-            { author: 'xxx', date: '27/02', hour: '16h00', text: 'fdsfsdfdsfsdf' },
-            { author: 'xxx', date: '27/02', hour: '16h00', text: 'fdsfsdfdsfsdf' }
-        ],
-        date: '17/06/2021',
-        hour: '15h30'
+        const topicTitle = document.createElement('h2');
+        topicTitle.innerHTML = `Titre du sujet ${topic.title}`;
+        topicDisplay.appendChild(topicTitle);
 
-    };
+        const topicDescription = document.createElement('h3');
+        topicDescription.innerHTML = `Posté le ${topic.date} à ${topic.hour} par ${topic.author}`;
+        topicDisplay.appendChild(topicDescription);
 
-    const topicTitle = document.createElement('h2');
-    topicTitle.innerHTML = `Titre du sujet ${topic.title}`;
-    topicDisplay.appendChild(topicTitle);
+        const refreshButton = document.createElement('button');
+        refreshButton.innerHTML = 'Actualiser les messages';
+        topicDisplay.appendChild(refreshButton);
 
-    const topicDescription = document.createElement('h3');
-    topicDescription.innerHTML = `Posté le ${topic.date} à ${topic.hour} par ${topic.author}`;
-    topicDisplay.appendChild(topicDescription);
+        const topicContainer = document.createElement('div');
 
-    const refreshButton = document.createElement('button');
-    refreshButton.innerHTML = 'Actualiser les messages';
-    topicDisplay.appendChild(refreshButton);
+        topic.messages.forEach((message) => {
+            const messageElement = createMessageElement(message);
+            topicContainer.appendChild(messageElement);
+        });
 
-    const topicContainer = document.createElement('div');
+        const messageForm = createMessageForm();
+        topicContainer.appendChild(messageForm);
+        topicDisplay.appendChild(topicContainer);
 
-    topic.messages.forEach((message) => {
-        const messageElement = createMessageElement(message);
-        topicContainer.appendChild(messageElement);
     });
-
-    const messageForm = createMessageForm();
-    topicContainer.appendChild(messageForm);
-    topicDisplay.appendChild(topicContainer);
 
     return topicDisplay;
 
