48381d06ec5aa0acf6d724d481b9c6cd0382793d
Blayn <jad.sebahi@gmail.com>
2021-06-18 14:36:54 +0200

    Update on feature/action-login

--- js/action-login.js
+++ js/action-login.js
@@ -7,17 +7,19 @@ myForm.addEventListener('submit', login);
 
 function login(event) {
     event.preventDefault();
-    const username = document.getElementsByName('username').value;
-    const password = document.getElementsByName('password').value;
+    const input_username = event.target.password.value;
+    const input_password = event.target.password.value;
 
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://localhost:8080/login', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({ username: 'Toto', password: '@z3rtY' }));
+    xhr.send(JSON.stringify({ username: input_username, password: input_password }));
     new Promise((resolve, reject) => {
-        xhr.onload = () => resolve(JSON.parse(xhr.response));
+        xhr.onload = () => (xhr.status !== 200) ? reject() : resolve(JSON.parse(xhr.response));
     }).then(user => {
         console.log(user);
         return user;
-    })
+    }).catch(() => {
+        console.log('Oops, user not found');
+    });
 }
